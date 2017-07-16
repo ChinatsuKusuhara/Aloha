4985fe11026887a6d5a6cd70a173225edd64d5d6
Chinatsu <chinatsu.k@hotmail.com>
2017-07-16 00:30:43 -0700

    keep working on carousel-cell on style css

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // carousel
+$(document).ready(function() {
+  var elem = document.querySelector('.main-carousel');
+
+  var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true
+  });
+
+// element argument can be a selector string
+//   for an individual element
+});
